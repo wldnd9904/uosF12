0f8e9f1e4e8d2fa6a222f40dde1866b4669f93f8
wldnd9904 <wldnd9904@uos.ac.kr>
2023-03-14 17:32:15 +0900

    Initial Commit

---
+++ uosF12/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  uosF12
+//
+//  Created by 최지웅 on 2023/03/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ uosF12/uosF12App.swift
@@ -0,0 +1,17 @@
+//
+//  uosF12App.swift
+//  uosF12
+//
+//  Created by 최지웅 on 2023/03/14.
+//
+
+import SwiftUI
+
+@main
+struct uosF12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
